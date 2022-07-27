a82bbcbecff43cbd39fdcfa56ce87c6a38d3c85b
Ömer Adıgüzel <77545818+OmerAdiguzel0@users.noreply.github.com>
2022-07-27 20:05:25 +0300

    Update Musteri.cs

--- MusteriTakip/Musteri.cs
+++ MusteriTakip/Musteri.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MusteriTakip
 {
-    public class Musteri:MusteriManager
+    public class Musteri
     {
         public int ID { get; set; }
         public string Ad { get; set; }
